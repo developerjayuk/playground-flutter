6204d68db6007679ff46d631035244c50e0bcd2c
Jason James <jason.james@cba-japan.com>
2025-09-25 22:34:32 +0100

    ittems transformer

--- hacker_news/lib/src/blocs/stories_bloc.dart
+++ hacker_news/lib/src/blocs/stories_bloc.dart
@@ -5,15 +5,35 @@ import "../resources/repository.dart";
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
+  late Stream<Map<int, Future<ItemModel>>> items;
   Stream<List<int>> get topIds => _topIds.stream;
+
+  Function(int) get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
+
+  _itemsTransformer() {
+    return ScanStreamTransformer((
+      Map<int, Future<ItemModel>> cache,
+      int id,
+      _,
+    ) {
+      cache[id] = _repository.fetchItem(id) as Future<ItemModel>;
+      return cache;
+    }, <int, Future<ItemModel>>{});
+  }
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
---
+++ hacker_news/lib/src/widgets/news_list_square.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({required this.itemId});
+
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still loading bruh!');
+        }
+        
+        return Text('Item: ${itemId}');
+      },
+    );
+  }
+}
