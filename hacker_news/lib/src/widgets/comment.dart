72b5d2bf5d7de9849d9dd534c21ba5bdb887ff83
Jason James <jason.james@cba-japan.com>
2025-10-02 22:50:23 +0100

    final tweaks

--- hacker_news/lib/src/widgets/comment.dart
+++ hacker_news/lib/src/widgets/comment.dart
@@ -1,13 +1,19 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
+import 'loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
   final Map<int, Future<ItemModel>> itemMap;
   final int depth;
 
-  const Comment({super.key, required this.itemId, required this.itemMap, required this.depth});
+  const Comment({
+    super.key,
+    required this.itemId,
+    required this.itemMap,
+    required this.depth,
+  });
 
   @override
   Widget build(context) {
@@ -15,7 +21,7 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still loading comment...');
+          return LoadingContainer();
         }
 
         final item = snapshot.data;
@@ -23,18 +29,20 @@ class Comment extends StatelessWidget {
         if (item != null) {
           final children = <Widget>[
             ListTile(
-              title: Text(item.text ?? ''),
+              title: buildText(item),
               subtitle: Text(item.by == '' ? 'Deleted' : item.by ?? 'n/a'),
               contentPadding: EdgeInsets.only(
                 right: 16.0,
-                left: (depth + 1) * 16.0
+                left: (depth + 1) * 16.0,
               ),
             ),
             Divider(),
           ];
 
           item.kids?.forEach((kidId) {
-            children.add(Comment(itemId: kidId, itemMap: itemMap, depth: depth + 1));
+            children.add(
+              Comment(itemId: kidId, itemMap: itemMap, depth: depth + 1),
+            );
           });
 
           return Column(children: children);
@@ -44,5 +52,14 @@ class Comment extends StatelessWidget {
         return SizedBox.shrink();
       },
     );
+  }
+
+  Widget buildText(ItemModel item) {
+    final text = item.text
+        ?.replaceAll('<p>', '\n\n')
+        .replaceAll('</p>', '')
+        .replaceAll('&#x27;', "'");
+
+    return Text(text ?? 'n/a');
   }
 }
